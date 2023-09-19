a293d2368a794a28730c45a8277db20001d1aa0d
Ropert Nathan <114857772+NathanRopert@users.noreply.github.com>
2023-09-19 09:31:38 -0400

    Creation du user BDD
    creation d'un utilisateur commun pour la base de données.

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Paramètres de base de données
-define('DB_HOST', ' ');
-define('DB_USER', ' ');
-define('DB_PASSWORD', ' ');
-define('DB_NAME', ' ');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'TestUserAdmin');
+define('DB_PASSWORD', '123');
+define('DB_NAME', 'wirefit');
 ?>
