4de48425a56f0016d6b74b5b426063ac8133281d
Julianoo <julien.quelin33@gmail.com>
2023-10-31 05:39:04 -0300

    Ajout du bouton déconnexion #25

--- src/app/views/profil.php
+++ src/app/views/profil.php
@@ -1,3 +1,43 @@
+<?php
+// Vérifier si l'utilisateur est connecté
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once('../models/Utilisateur.php');
+require_once('../controllers/UtilisateurController.php');
+session_start();
+$utilisateurController = new UtilisateurController($mysqli);
+$utilisateurObjet = $_SESSION['utilisateur'];
+$emailUtilisateur = $utilisateurObjet->getEmail();
+$nomUtilisateur = $utilisateurObjet->getNom();
+$prenomUtilisateur = $utilisateurObjet->getPrenom();
+$adresseUtilisateur = $utilisateurObjet->getAdresse();
+
+//si le formulaire est envoyer
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $emailUtilisateur = $_POST['email'];
+    $nomUtilisateur = $_POST['first_name'];
+    $prenomUtilisateur = $_POST['last_name'];
+    $adresseUtilisateur = $_POST['address'];
+    $ModifiactionUtilisateur =  $utilisateurController->modificationUtilisateur($emailUtilisateur, $nomUtilisateur, $prenomUtilisateur, $adresseUtilisateur);
+    $_SESSION['utilisateur']->setEmail($emailUtilisateur);
+    $_SESSION['utilisateur']->setNom($nomUtilisateur);
+    $_SESSION['utilisateur']->setPrenom($prenomUtilisateur);
+    $_SESSION['utilisateur']->setAdresse($adresseUtilisateur);
+    if(isset($_POST['password']) && !empty($_POST['password'])){
+        $passwordUtilisateur = $_POST['password'];
+        $passwordUtilisateur = $utilisateurController->modificationMotDePasse($emailUtilisateur, $passwordUtilisateur);
+    }  
+}
+
+if(isset($_POST['deconnexion'])) {
+    session_unset();
+    session_destroy();
+    header('Location: ./connexion.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -32,33 +72,33 @@
                 <div id="nom-prenom">
                     <div class="champ">
                         <p>Nom</p>
-                        <input type="text" readonly>
+                        <input type="text" id="first_name" name="first_name" value="<?php echo $nomUtilisateur?>" readonly>
                     </div>
                     <div class="champ" id="prenom">
                         <p>Prénom</p>
-                        <input type="text" readonly>
+                        <input type="text" id="last_name" name="last_name" value="<?php echo $prenomUtilisateur ?>" readonly>
                     </div>
                 </div>
                 <div id="mail">
                     <div class="champ">
-                        <p>Addresse mail</p>
-                        <input type="text" readonly>
+                        <p>Adresse mail</p>
+                        <input type="email" id="email" name="email" value="<?php echo $emailUtilisateur ?>" readonly>
                     </div>
                 </div>
                 <div id="adresse">
                     <div class="champ">
-                        <p>Addresse</p>
-                        <input type="text" readonly>
+                        <p>Adresse</p>
+                        <input type="text" id="address" name="address" value="<?php echo $adresseUtilisateur?>" readonly>
                     </div>
                 </div>
                 <div id="code-postal-ville">
                     <div class="champ">
-                        <p>Code postal</p>
-                        <input type="text" readonly>
+                        <p>Mot de passe actuelle</p>
+                        <input type="password" id="password" name="password" value="<?php ?>" readonly>
                     </div>
                     <div class="champ" id="ville">
-                        <p>Ville</p>
-                        <input type="text" readonly>
+                        <p>Nouveau mot de passe</p>
+                        <input type="password" id="password" name="password" value="<?php ?>" readonly>
                     </div>
                 </div>
             </div>
@@ -67,6 +107,9 @@
             <a href="./editerProfil.php">
                 <input type="button" value="Editer le profil">
             </a>
+            <form method="post">
+                <input type="submit" name="deconnexion" value="Déconnexion">
+            </form>
         </div>
     </div>
 </body>
