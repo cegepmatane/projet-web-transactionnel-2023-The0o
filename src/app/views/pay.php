557f6748535f3af1de98b818a1c143210db7d768
The0o <theolaidin@gmail.com>
2023-11-01 12:52:23 -0400

    Revert "Ajout paiement en ligne #34"
    This reverts commit d42ab422dcd5f0f891fc9fcf13a4a065e63fc71c.

--- src/app/views/pay.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'StripePayment.php';
-$payment = new StripePayment('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');
-$payment->startPayment();
-?>
