37b930fa15343a0b8dce8c7dafbd217158ea1cb0
Julianoo <julien.quelin33@gmail.com>
2023-12-06 18:25:09 -0400

    Suppression affichage de la commande #43

--- src/app/views/ajoutProduitBDD.php
+++ src/app/views/ajoutProduitBDD.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Valider et ajouter le produit à la base de données (vous devez implémenter cette logique)
     if ($nom && $prix !== false) {
         // Ajouter le produit à la base de données en utilisant la méthode appropriée de ProduitController
-        echo $produitController->ajouterUnProduit($nom, $prix, $description, $marque, $reduction, $sexe, $afficher, $type, $imagesUn, $imagesDeux);
+        $produitController->ajouterUnProduit($nom, $prix, $description, $marque, $reduction, $sexe, $afficher, $type, $imagesUn, $imagesDeux);
 
         // Mettre à jour la réponse en cas de succès
         $response['success'] = true;
